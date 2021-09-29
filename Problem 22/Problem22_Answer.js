1d34cf86cdde5cb5b5c1be9400ce2a347f4a7b7f
Andrew Brittenham <sirandrewking.git@gmail.com>
2021-09-29 18:03:44 -0500

    Worked this at rhino. closest output

--- Problem 22/Problem22_Answer.js
+++ Problem 22/Problem22_Answer.js
@@ -13,3 +13,43 @@ So, COLIN would obtain a score of 938 × 53 = 49714.
 
 What is the total of all the name scores in the file?
 */
+
+
+let nameList = require('./ex_names').sort() //1st: sort list
+
+// A-Z = 1-26...ASCII A=65 - Z=90
+function alphaNum(arg) {
+
+	let character = arg;
+	num = character.charCodeAt(0) - 64;
+	return num;
+};
+
+
+
+function shiftedArr(arg) {
+	let name = '';
+	let letterVal = 0;
+	//console.log('this was removed:', shifted)
+	for (let i = 0; i < nameList.length; i++) {
+		let name = nameList[i]
+		nameList[i]
+		for (let j = 0; j < name.length; j++) {
+			console.log(name[j])
+			letterVal += alphaNum(name[j])
+			
+		};	
+		console.log(letterVal, "here")
+	};
+};
+
+
+
+
+//console.log(character)
+//shiftedArr(alphaNum)
+//console.log(nameList)
+
+shiftedArr(nameList)
+//console.log(nameList)
+//console.log('Length of list:', nameList.length)
